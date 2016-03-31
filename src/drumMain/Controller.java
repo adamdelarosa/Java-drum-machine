39120254f500e94c6d5af56536b54a68f0ba8534
ROSA <gojiradam@gmail.com>
2016-03-31 17:57:30 +0200

    Drums add

--- src/drumMain/Controller.java
+++ src/drumMain/Controller.java
@@ -1,10 +1,21 @@
 package drumMain;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.InputStream;
+
 public class Controller {
 
-    public void test(){
-        System.out.println("Yoo");
+    public void padOne(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Server/Sound/switch.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
 }

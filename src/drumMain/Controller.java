d6b3c7d69859f1e023204660fc1bf1419ab8f477
ROSA <gojiradam@gmail.com>
2016-03-31 18:13:54 +0200

    Button config

--- src/drumMain/Controller.java
+++ src/drumMain/Controller.java
@@ -9,7 +9,171 @@ public class Controller {
 
     public void padOne(){
         try {
-            InputStream is = ClassLoader.getSystemResourceAsStream("Server/Sound/switch.wav");
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/kick1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padTwo(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/fx1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padThree(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/hh1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padFour(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/hh2.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padFive(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/hh3.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padSix(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/crash1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padSeven(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/ride1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padEight(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/snare1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }public void padNine(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/snare2.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padTen(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/snare3.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padEleven(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/synth1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padTwelve(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/synth2.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padThirteen(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/synth3.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padFourteen(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/synth4.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padFifteen(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/tom1.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void padSixteen(){
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("Samples/808/tom2.wav");
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(is);
             Clip clip = AudioSystem.getClip();
             clip.open(audioIn);
